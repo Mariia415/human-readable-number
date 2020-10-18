a9f600713f03354cd5e6b0dc77199106f124807f
Mariia415 <mariia.galitsyna@gmail.com>
2020-10-18 09:52:12 +0200

    update: first try

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable (number) { 
+    let readableNumbers = {
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four'
+    };
+    return readableNumbers[number];
   
-}
+};
